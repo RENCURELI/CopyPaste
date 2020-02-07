951ac02ec425e7d7455645ce9508c3c9ad8b0c20
RENCUREL <pierre.rencurel@orange.fr>
2020-02-06 21:48:29 -0500

    Copy Paste
    DONE : Copy and paste from one folder to another
    TODO : Test erase and add interface / comments

--- CopyPasteProject/CopyPasteProject/Program.cs
+++ CopyPasteProject/CopyPasteProject/Program.cs
@@ -17,16 +17,69 @@ namespace CopyPasteProject
         {
             string target;
             string source;
-
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Hello World! \n" +
+                "Bienvenu dans le programme de synchronisation");
+            
             string curFile = Environment.CurrentDirectory + @"\Ini.txt";
             if (File.Exists(curFile))
             {
-                foreach (string file in GetFiles(File.ReadLines(Environment.CurrentDirectory + @"\Ini.txt").First()))
-                {
-                    Console.WriteLine(file);
-                }
+                target = File.ReadLines(Environment.CurrentDirectory + @"\Ini.txt").Skip(1).Take(1).First();
+                source = File.ReadLines(Environment.CurrentDirectory + @"\Ini.txt").First();
 
+                int value = Console.Read();
+                char select = Convert.ToChar(value);
+
+                Console.WriteLine(select);
+
+                switch (select)
+                {
+                    case '1':
+                        Console.WriteLine("Enter Target path");
+                        target = Console.ReadLine();
+
+                        Console.WriteLine("Enter Source path");
+                        source = Console.ReadLine();
+
+                        WriteFile(source, target);
+                        break;
+
+                    case '2':
+                        //Copy target to source
+                        foreach (string file in GetFiles(target))
+                        {
+                            File.Copy(file, Path.Combine(source, Path.GetFileName(file)), true);
+                        }
+                        break;
+
+                    case '3':
+                        //Copy source to target
+                        foreach (string file in GetFiles(source))
+                        {
+                            File.Copy(file, Path.Combine(target, Path.GetFileName(file)), true);
+                        }
+                        break;
+
+                    case '4':
+                        //List files in source
+                        foreach (string file in GetFiles(source))
+                        {
+                            Console.WriteLine(file);
+                        }
+                        break;
+
+                    case '5':
+                        //List files in target
+                        foreach (string file in GetFiles(target))
+                        {
+                            Console.WriteLine(file);
+                        }
+                        break;
+
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
             }
             else
             {
@@ -38,45 +91,6 @@ namespace CopyPasteProject
 
                 WriteFile(source, target);
             }
-
-            int select = Console.Read();
-
-            switch (select)
-            {
-                case 1:
-                    Console.WriteLine("Enter Target path");
-                    target = Console.ReadLine();
-
-                    Console.WriteLine("Enter Source path");
-                    source = Console.ReadLine();
-
-                    WriteFile(source, target);
-                    break;
-
-                case 2:
-                    //Copy target to source
-                    break;
-
-                case 3:
-                    //Copy source to target
-                    break;
-
-                case 4:
-                    //List files in source
-                    break;
-
-                case 5:
-                    //List files in target
-                    break;
-
-                default:
-                    Environment.Exit(0);
-                    break;
-            }
-
-            Console.ReadKey();
-            
-
             Console.ReadKey();
         }
         
