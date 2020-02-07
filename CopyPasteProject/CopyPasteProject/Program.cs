55b1375a0ab0cf7effed96dd4f72c66519ea4f5f
RENCUREL <pierre.rencurel@orange.fr>
2020-02-07 11:39:37 -0500

    Loops
    DONE : Commented and started implementing loops
    TODO : Flush input stream

--- CopyPasteProject/CopyPasteProject/Program.cs
+++ CopyPasteProject/CopyPasteProject/Program.cs
@@ -11,86 +11,106 @@ namespace CopyPasteProject
     
     class Program
     {
-        string[] directories = Directory.GetDirectories("D:\\");
         
         static void Main(string[] args)
         {
+            //Path to target folder
             string target;
+            //Path to source folder
             string source;
             
             Console.WriteLine("Hello World! \n" +
                 "Bienvenue dans le programme de synchronisation");
             
+            //Check if specified initialisation file exists
             string curFile = Environment.CurrentDirectory + @"\Ini.txt";
             if (File.Exists(curFile))
             {
+                //Save folder paths to specified variables
                 target = File.ReadLines(Environment.CurrentDirectory + @"\Ini.txt").Skip(1).Take(1).First();
                 source = File.ReadLines(Environment.CurrentDirectory + @"\Ini.txt").First();
 
-                Console.WriteLine("Entre la commande voulue :\n" +
-                    "1 = Mettre à jour chemin de dossier \n" +
-                    "2 = Copier de la Cible à la Source \n" +
-                    "3 = Copier de la Source à la Cible \n" +
-                    "4 = Lister contenu de la Source \n" +
-                    "5 = Lister contenu de la Cible \n" +
-                    "6 = Femer programme");
-
-                int value = Console.Read();
-                char select = Convert.ToChar(value);
-
-                Console.WriteLine(select);
-
-                switch (select)
+                Console.WriteLine("Pour commencer, appuyez sur Enter");
+                while (Console.ReadKey().Key != ConsoleKey.N)
                 {
-                    case '1':
-                        Console.WriteLine("Enter Target path");
-                        target = Console.ReadLine();
 
-                        Console.WriteLine("Enter Source path");
-                        source = Console.ReadLine();
+                    //Enter command to select operation
+                    Console.WriteLine("Entrez la commande voulue :\n" +
+                        "1 = Mettre à jour chemin de dossier \n" +
+                        "2 = Copier de la Cible à la Source \n" +
+                        "3 = Copier de la Source à la Cible \n" +
+                        "4 = Lister contenu de la Source \n" +
+                        "5 = Lister contenu de la Cible \n" +
+                        "6 = Femer programme");
 
-                        WriteFile(source, target);
-                        break;
+                    //Read char's ASCII value
+                    int value = Console.Read();
+                    //Convert value to Char
+                    char select = Convert.ToChar(value);
+                    Console.ReadKey();
 
-                    case '2':
-                        //Copy target to source
-                        foreach (string file in GetFiles(target))
-                        {
-                            File.Copy(file, Path.Combine(source, Path.GetFileName(file)), true);
-                        }
-                        break;
+                    //Operation selection
+                    switch (select)
+                    {
+                        case '1':
+                            //Update save paths
+                            Console.WriteLine("Enter Target path");
+                            target = Console.ReadLine();
 
-                    case '3':
-                        //Copy source to target
-                        foreach (string file in GetFiles(source))
-                        {
-                            File.Copy(file, Path.Combine(target, Path.GetFileName(file)), true);
-                        }
-                        break;
+                            Console.WriteLine("Enter Source path");
+                            source = Console.ReadLine();
 
-                    case '4':
-                        //List files in source
-                        foreach (string file in GetFiles(source))
-                        {
-                            Console.WriteLine(file);
-                        }
-                        break;
+                            WriteFile(source, target);
+                            break;
 
-                    case '5':
-                        //List files in target
-                        foreach (string file in GetFiles(target))
-                        {
-                            Console.WriteLine(file);
-                        }
-                        break;
+                        case '2':
+                            //Copy target to source
+                            foreach (string file in GetFiles(target))
+                            {
+                                File.Copy(file, Path.Combine(source, Path.GetFileName(file)), true);
+                                Console.WriteLine("Copied : " + file);
+                            }
+                            break;
 
-                    default:
-                        Environment.Exit(0);
-                        break;
+                        case '3':
+                            //Copy source to target
+                            foreach (string file in GetFiles(source))
+                            {
+                                File.Copy(file, Path.Combine(target, Path.GetFileName(file)), true);
+                                Console.WriteLine("Copied : " + file);
+                            }
+                            break;
+
+                        case '4':
+                            //List files in source
+                            foreach (string file in GetFiles(source))
+                            {
+                                Console.WriteLine(file);
+                            }
+                            break;
+
+                        case '5':
+                            //List files in target
+                            foreach (string file in GetFiles(target))
+                            {
+                                Console.WriteLine(file);
+                            }
+                            break;
+
+                        default:
+                            //Exit program
+                            Environment.Exit(0);
+                            break;
+                    }
+
+                
+                    Console.WriteLine("Thank you :) \n" +
+                    "Voulez vous continuer y / n ?");
                 }
             }
             else
             {
+                //If file does not exist, enter paths and generate file
                 Console.WriteLine("Enter Target path (format C:\\Documents'\'...'\'dossier final)");
                 target = Console.ReadLine();
 
@@ -99,18 +119,20 @@ namespace CopyPasteProject
 
                 WriteFile(source, target);
             }
+            
             Console.ReadKey();
         }
         
 
         static IEnumerable<string> GetFiles(string path)
         {
+            //Queue of files in folder
             Queue<string> queue = new Queue<string>();
             queue.Enqueue(path);
             while (queue.Count > 0)
             {
                 path = queue.Dequeue();
-                try
+                try //Get exceptions
                 {
                     foreach (string subDir in Directory.GetDirectories(path))
                     {
@@ -124,6 +146,7 @@ namespace CopyPasteProject
                 string[] files = null;
                 try
                 {
+                    //Get paths to files in directory
                     files = Directory.GetFiles(path);
                 }
                 catch (Exception ex)
@@ -132,6 +155,7 @@ namespace CopyPasteProject
                 }
                 if (files != null)
                 {
+                    //Output files and paths
                     for (int i = 0; i < files.Length; i++)
                     {
                         yield return files[i];
@@ -142,6 +166,7 @@ namespace CopyPasteProject
 
         static void WriteFile(string src, string tgt)
         {
+            //Generate file and enter path strings
             string[] lines = { src, tgt };
             File.WriteAllLines(Environment.CurrentDirectory + @"\Ini.txt", lines);
         }
